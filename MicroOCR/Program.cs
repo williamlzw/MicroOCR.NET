128f5829265dfa8e5b7e9ecf35f789012631626d
william <313620225@qq.com>
2022-11-03 11:16:23 +0800

    update

--- MicroOCR/Program.cs
+++ MicroOCR/Program.cs
@@ -9,31 +9,7 @@ namespace MicroOCR
             var cfg = BuildCfg();
             TrainModel(cfg);
         }
-        //public static void test()
-        //{
-        //    torchvision.io.DefaultImager = new torchvision.io.SkiaImager();
-        //    var image = torchvision.io.read_image("d:\\dataset\\train\\00000.jpg");
-        //    Console.WriteLine(image.dtype);
 
-        //    foreach (var index in image.shape)
-        //    {
-        //        Console.WriteLine(index);
-        //    }
-        //    var img = Cv2.ImRead("d:\\dataset\\train\\00000.jpg");
-        //    Cv2.CvtColor(img, img, ColorConversionCodes.BGR2RGB);
-        //    var bytes = new byte[img.Total() * 3];
-        //    Marshal.Copy(img.Data, bytes, 0, bytes.Length);
-        //    var imgTensor = torch.tensor(bytes, torch.uint8).reshape(3, 30, -1);
-        //    Console.WriteLine(imgTensor.dtype);
-        //    foreach (var index in imgTensor.shape)
-        //    {
-        //        Console.WriteLine(index);
-        //    }
-        //    var aa = image.equal(imgTensor);
-        //    Console.WriteLine(image.ToString(TorchSharp.TensorStringStyle.Julia));
-        //    Console.WriteLine(imgTensor.ToString(TorchSharp.TensorStringStyle.Julia));
-        //    Console.WriteLine(aa.ToString(TorchSharp.TensorStringStyle.Julia));
-        //}
         public static Dictionary<CfgNode, object> BuildCfg()
         {
             Dictionary<CfgNode, object> cfg = new Dictionary<CfgNode, object>();
